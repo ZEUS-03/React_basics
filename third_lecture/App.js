a81a2374e4561ec24d49c775cfe5ec693dd09494
Gautam Sharma <gautam332000lavanya@gmail.com>
2023-03-14 00:02:46 +0530

    final update on third_lecture.

--- third_lecture/App.js
+++ third_lecture/App.js
@@ -1,39 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React.createElement => Object =>HTML(DOM)
+// using JSX
+// JSX => createElement => Object =>HTML(DOM)
 
-// Using React.js
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading",
-    key: "yes"
-  },
-  "Hello World!"
-);
+const heading = <h1 id="heading" key="heading1">Namaste React</h1> //Element
+const Heading1 = () => {
+  return (
+    <h3>
+      This is used to show how to import one component inside another.
+    </h3>
+  )
+}
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "heading2",
-    key: "world"
-  },
-  "Hello World again!"
-);
-
-const container = React.createElement(
-  "div",
-  {
-    id: "container"
-  },
-  [heading, heading2]
-);
+const Heading2 = () => {
+  return (
+    <div>
+      <h1>
+        This is the heading no. 1.
+      </h1>
+      <h2>
+        This is heading no. 2.
+      </h2>
+      {heading /*importing as Element inside jsx*/} 
+      {Heading1() /*importing as Function inside jsx*/}
+      <Heading1 /*importing as a component*/ /> 
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<Heading2 />);
 
-// Using Vanilla JS
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Hello World!";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
+// Using React.js
+
+// const heading = React.createElement(
+  //   "h1",
+  //   {
+  //     id: "heading",
+  //     key: "h1"
+  //   },
+  //   "Hello World!"
+  // );
+  
+  // console.log(heading);
+  
+  // const heading2 = React.createElement(
+  //   "h2",
+  //   {
+  //     id: "heading2",
+  //     key: "h2"
+  //   },
+  //   "Hello World again!"
+  // );
+  
+  // const container = React.createElement(
+  //   "div",
+  //   {
+  //     id: "container"
+  //   },
+  //   [heading, heading2]
+  // );
--- third_lecture/UPDATES.md
+++ third_lecture/UPDATES.md
@@ -1,5 +1,19 @@
 # Learnings and updates from the last code:
 ### Learnings:
-> Babel: 
->> package that handles and perform polyfill(a technique to convert new code to broser compatible code) so that the code can work in older browser.
-> `Tree Shaking`: Performed by Parcel(bundlers) which results in removal of unwanted code(unused functions that came up with the libraries) from our project. 
+
+> **`Dev Dependencies`** and **`Normal Depeandencies`** :
+> `Dev Dependencies` are those which are required in dev environment only such as *Parcel*. Whereas `Normal Dependencies` are dependencies that are required by our project to run such as *React*.
+
+> **`Babel`**: 
+> A package that is a compiler thathandles and generates polyfill(old piece of code converted from new code by parcel for browser compatiblility) so that the code can work in older browser. It also compiles the code written in `JSX` via using React.createElement in the background(refer to the official babel's documentation.)
+
+> **`Tree Shaking`**: 
+> Performed by Parcel(bundlers) which results in removal of unwanted code(unused functions that came up with the libraries) from our project. 
+
+> **`Keys`**:
+> `Keys` in React helps React render only particular element(in siblings). 
+Keys help React identify which items have changed, are added, or are removed.Without keys, React takes a lot of efforts as it rerenders the complete page just to add or update a sibling.
+
+> **`JSX`**:
+> `JSX` helps in writing clean, readable and maintainable code. JSX is a syntax extension to JS. JSX has an **HTML like syntax** but it's not HTML inside Javascript. It is based on createElement in the background and uses babel as compiler to convert the code from this syntax to createElement format (normal JS option).
+
