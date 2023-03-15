e9adbc7a7645135bc63f2e7d00dad5474e605fc5
Gautam Sharma <gautam332000lavanya@gmail.com>
2023-03-15 13:05:53 +0530

    files updated successfully!

--- foodies_project/App.js
+++ foodies_project/App.js
@@ -22,7 +22,7 @@ const Header = () => {
   return (
     <div className="header-container">
       <a href="index.html">
-        <img alt="logo" src="https://th.bing.com/th/id/OIP.p4Ve--D7nEaiaItSfMCPFQHaE8?pid=ImgDet&rs=1" />
+        <img className="logo" alt="logo" src="https://th.bing.com/th/id/OIP.p4Ve--D7nEaiaItSfMCPFQHaE8?pid=ImgDet&rs=1" />
       </a>
       <ul>
         <li>Restaurants</li>
@@ -34,16 +34,690 @@ const Header = () => {
   );
 };
 
-// const Body = () => {
-//   return (
-//     >
-//   );
-// };
+const RestaurantList = [
+  {
+    "info": {
+      "id": "129366",
+      "name": "Ganesh Bhojnalaya - Jyoti Nagar",
+      "cloudinaryImageId": "ebqewymo7zx4kdsslxdw",
+      "locality": "Jyoti Nagar",
+      "areaName": "Lal Kothi",
+      "costForTwo": "₹200 for two",
+      "cuisines": [
+        "North Indian",
+        "Thalis",
+        "Chinese",
+        "Mughlai",
+        "Chaat",
+        "Punjabi",
+        "Desserts",
+        "Snacks",
+        "Rajasthani",
+        "Tandoor",
+        "Sweets",
+        "Indian",
+        "Beverages",
+        "Ice Cream",
+        "Italian",
+        "Pastas"
+      ],
+      "avgRating": 4.1,
+      "veg": true,
+      "feeDetails": {
+        "restaurantId": "129366",
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 2700
+          },
+          {
+            "name": "BASE_TIME"
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 2700
+      },
+      "parentId": "85926",
+      "avgRatingString": "4.1",
+      "totalRatingsString": "10K+",
+      "promoted": true,
+      "adTrackingId": "cid=6191058~p=1~eid=00000186-e3c7-115c-02e7-bb9600ec0108~45995",
+      "sla": {
+        "deliveryTime": 28,
+        "lastMileTravel": 3.7,
+        "serviceability": "SERVICEABLE",
+        "slaString": "28 mins",
+        "lastMileTravelString": "3.7 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-03-15 16:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "textBased": {
+            
+          },
+          "imageBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "60% OFF",
+        "subHeader": "UPTO ₹120",
+        "discountCalloutInfo": {
+          "message": "Free Delivery",
+          "logoCtx": {
+            "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+          }
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT"
+    }
+  },
+  {
+    "info": {
+      "id": "443546",
+      "name": "cakes",
+      "cloudinaryImageId": "ebpu7eofpezoz5dfi0wx",
+      "locality": "Hera path",
+      "areaName": "Mansarovar",
+      "costForTwo": "₹600 for two",
+      "cuisines": [
+        "Bakery"
+      ],
+      "veg": true,
+      "feeDetails": {
+        "restaurantId": "443546",
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 2000
+          },
+          {
+            "name": "BASE_TIME"
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 2000
+      },
+      "parentId": "4861",
+      "avgRatingString": "--",
+      "sla": {
+        "deliveryTime": 25,
+        "lastMileTravel": 0.3,
+        "serviceability": "SERVICEABLE",
+        "slaString": "25 mins",
+        "lastMileTravelString": "0.3 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-03-15 23:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          },
+          "textBased": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "discountCalloutInfo": {
+          "message": "Free Delivery",
+          "logoCtx": {
+            "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+          }
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT"
+    }
+  },
+  {
+    "info": {
+      "id": "257747",
+      "name": "DMB Sweets Pvt Ltd",
+      "cloudinaryImageId": "kolhcnu4vpylabtjemll",
+      "locality": "Kartarpura",
+      "areaName": "Bais Godam",
+      "costForTwo": "₹350 for two",
+      "cuisines": [
+        "Sweets",
+        "North Indian",
+        "Snacks",
+        "South Indian",
+        "Chinese",
+        "Bengali",
+        "Italian",
+        "Bakery"
+      ],
+      "avgRating": 4.3,
+      "veg": true,
+      "feeDetails": {
+        "restaurantId": "257747",
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 2000
+          },
+          {
+            "name": "BASE_TIME"
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 2000
+      },
+      "parentId": "244910",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "10K+",
+      "sla": {
+        "deliveryTime": 26,
+        "lastMileTravel": 2.5,
+        "serviceability": "SERVICEABLE",
+        "slaString": "26 mins",
+        "lastMileTravelString": "2.5 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-03-15 21:30:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "textBased": {
+            
+          },
+          "imageBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "50% OFF",
+        "subHeader": "UPTO ₹100",
+        "discountCalloutInfo": {
+          "message": "Free Delivery",
+          "logoCtx": {
+            "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+          }
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT"
+    }
+  },
+  {
+    "info": {
+      "id": "392347",
+      "name": "Rawat Misthan Bhandar - Sodala",
+      "cloudinaryImageId": "rjlyrjx6murpb741uzvn",
+      "areaName": "Shyam Nagar",
+      "costForTwo": "₹300 for two",
+      "cuisines": [
+        "Bakery",
+        "Sweets",
+        "South Indian",
+        "North Indian",
+        "Chinese"
+      ],
+      "avgRating": 4,
+      "veg": true,
+      "feeDetails": {
+        "restaurantId": "392347",
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 2000
+          },
+          {
+            "name": "BASE_TIME"
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 2000
+      },
+      "parentId": "253544",
+      "avgRatingString": "4.0",
+      "totalRatingsString": "1K+",
+      "sla": {
+        "deliveryTime": 24,
+        "lastMileTravel": 2.3,
+        "serviceability": "SERVICEABLE",
+        "slaString": "24 mins",
+        "lastMileTravelString": "2.3 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-03-15 22:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "textExtendedBadges": {
+            
+          },
+          "textBased": {
+            
+          },
+          "imageBased": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "discountCalloutInfo": {
+          "message": "Free Delivery",
+          "logoCtx": {
+            "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+          }
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT"
+    }
+  },
+  {
+    "info": {
+      "id": "65247",
+      "name": "Saini Dhaba - Sodala",
+      "cloudinaryImageId": "eyqrmzz7ctlhil4pt7gi",
+      "locality": "New Sanganer Road",
+      "areaName": "Sodala",
+      "costForTwo": "₹150 for two",
+      "cuisines": [
+        "North Indian",
+        "Beverages"
+      ],
+      "avgRating": 4.1,
+      "veg": true,
+      "feeDetails": {
+        "restaurantId": "65247",
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 2000
+          },
+          {
+            "name": "BASE_TIME"
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 2000
+      },
+      "parentId": "175724",
+      "avgRatingString": "4.1",
+      "totalRatingsString": "10K+",
+      "promoted": true,
+      "adTrackingId": "cid=6191056~p=3~eid=00000186-e3c7-115c-02e7-bb9800ec0344~45995",
+      "sla": {
+        "deliveryTime": 19,
+        "lastMileTravel": 1.8,
+        "serviceability": "SERVICEABLE",
+        "slaString": "19 mins",
+        "lastMileTravelString": "1.8 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-03-15 23:59:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "textBased": {
+            
+          },
+          "imageBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "60% OFF",
+        "subHeader": "UPTO ₹120",
+        "discountCalloutInfo": {
+          "message": "Free Delivery",
+          "logoCtx": {
+            "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+          }
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT"
+    }
+  },
+  {
+    "info": {
+      "id": "61560",
+      "name": "Bombay Mishthan Bhandar (BMB) - Durapura",
+      "cloudinaryImageId": "127d6d2a1a4f3e995fba2c5dd93b574f",
+      "locality": "Jai Jawan Colony",
+      "areaName": "Durgapura",
+      "costForTwo": "₹300 for two",
+      "cuisines": [
+        "North Indian",
+        "Thalis",
+        "Rajasthani",
+        "Snacks",
+        "Sweets",
+        "Indian"
+      ],
+      "avgRating": 4.2,
+      "veg": true,
+      "feeDetails": {
+        "restaurantId": "61560",
+        "fees": [
+          {
+            "name": "BASE_DISTANCE",
+            "fee": 4800
+          },
+          {
+            "name": "BASE_TIME"
+          },
+          {
+            "name": "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        "totalFee": 4800
+      },
+      "parentId": "13728",
+      "avgRatingString": "4.2",
+      "totalRatingsString": "10K+",
+      "sla": {
+        "deliveryTime": 28,
+        "lastMileTravel": 6,
+        "serviceability": "SERVICEABLE",
+        "slaString": "28 mins",
+        "lastMileTravelString": "6.0 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2023-03-15 23:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "textBased": {
+            
+          },
+          "imageBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "50% OFF",
+        "subHeader": "UPTO ₹100",
+        "discountCalloutInfo": {
+          "message": "Free Delivery",
+          "logoCtx": {
+            "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+          }
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT"
+    }
+  }];
+
+const RestaurantCard = ({name, cuisines, lastMileTravelString, cloudinaryImageId /** restaurant */}) => {
+  // const {name, cuisines, lastMileTravelString, cloudinaryImageId} = restaurant; using restaurant
+  return (
+    <div className="card">
+      <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + cloudinaryImageId} />
+      <h2>{name}</h2>
+      <h3>{cuisines.join(" ,")}</h3>
+      <h3>{lastMileTravelString}</h3>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      {/* <RestaurantCard name={RestaurantList[0].info.name} cuisines={RestaurantList[0].info.cuisines}/> */}
+      {/* <RestaurantCard {...RestaurantList[0].info}/> */}
+      {RestaurantList.map((restaurant) => {
+        return <RestaurantCard {...restaurant.info}/>
+      })}
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <>
       <Header />
+      <Body />
     </>
   );
 };
